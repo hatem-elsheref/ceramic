647f5b3b20c36e2343a46c86602f58d12b6336c2
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2026-01-27 03:19:12 +0200

    fixx

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-              ClearAllDataSeeder::class,  // ⚠️ WARNING: Uncomment to clear ALL data from ALL tables first
+              //ClearAllDataSeeder::class,  // ⚠️ WARNING: Uncomment to clear ALL data from ALL tables first
               ClearProductsSeeder::class,  // Uncomment to clear old products first
              AdminRoleTable::class,
              AdminTable::class,
